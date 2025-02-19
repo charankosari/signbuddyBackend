717ad88bd9044b57e74c704064e23fc440cdfd7f
charankosari <shivacharankosari099@gmail.com>
2025-02-19 19:21:23 +0530

    added more things to a user

--- models/userModel.js
+++ models/userModel.js
@@ -3,57 +3,85 @@ const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const { type } = require("os");
 
 const userSchema = new mongoose.Schema({
   userName: {
     type: String,
-    required: [false, "Please Enter  Username"],
-    maxlength: [40, "username should not exceed morethan 40 characters"],
-    minlength: [4, "username should not be lessthan 4 characters"],
+    required: [false, "Please Enter Username"],
+    maxlength: [40, "Username should not exceed 40 characters"],
+    minlength: [4, "Username should not be less than 4 characters"],
   },
   email: {
     type: String,
     required: [true, "Please Enter User Email"],
     unique: true,
-    validate: [validator.isEmail, "Please enter valid email"],
+    validate: [validator.isEmail, "Please enter a valid email"],
   },
-
-  password: { type: String, required: true, select: false },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
   avatar: {
     type: String,
     required: false,
   },
-
   role: {
     type: String,
     default: "user",
+  },
+  credits: {
+    type: Number,
+    default: 0,
+  },
+  subscriptionType: {
+    type: String,
+    enum: ["free", "premium", "enterprise"],
+    default: "free",
+  },
+  cooldownPeriod: {
+    type: Date,
+    default: 0,
+  },
+  documentsSent: {
+    type: Number,
+    default: 0,
+    validate: {
+      validator: function (val) {
+        if (this.subscriptionType === "free") {
+          return val <= 3;
+        }
+        return true;
+      },
+      message: "Free subscription users can send a maximum of 3 documents.",
+    },
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
 
-// pre hook to check weather password is modified
+// Pre-hook to hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    next();
+    return next();
   }
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-// generate Jwttoken
+// Generate JWT token
 userSchema.methods.jwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.jwt_secret, {
-    expiresIn: process.env.jwt_epire,
+    expiresIn: process.env.jwt_expire,
   });
 };
 
-// password compare
+// Compare password
 userSchema.methods.comparePassword = async function (password) {
-  console.log(password, this.password);
   return await bcrypt.compare(password, this.password);
 };
 
+// Generate password reset token
 userSchema.methods.resetToken = function () {
   const token = crypto.randomBytes(20).toString("hex");
   const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
