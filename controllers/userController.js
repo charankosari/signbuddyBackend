b980f50d0af8e7d96e96303a6c1dc1dbbaf21295
charankosari <shivacharankosari099@gmail.com>
2025-02-26 11:16:00 +0530

    edited send agreement

--- controllers/userController.js
+++ controllers/userController.js
@@ -889,7 +889,7 @@ exports.sendAgreement = asyncHandler(async (req, res, next) => {
       const docUrl = docUpload.url;
 
       const options = {
-        density: 72, // Increase DPI for better clarity
+        density: 10000, // Increase DPI for better clarity
         saveFilename: uniqueId,
         savePath: path.dirname(tempFilePath),
         format: "jpeg",
